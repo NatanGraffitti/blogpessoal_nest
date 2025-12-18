bf073be4147f85a2fc909dc97c57670c03bb5418
natan <natangraffi@hotmail.com>
2025-12-18 10:23:25 -0300

    (fix) resolvido conex

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { AppController } from './app.controller';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: 'root',
