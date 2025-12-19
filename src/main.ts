fa3540cb82596560e0b5b87f399574b87601f2ec
natan <natangraffi@hotmail.com>
2025-12-19 11:23:31 -0300

    fix: ajuste de porta

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
