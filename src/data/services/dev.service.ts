1f85053caed92602d04211ee154af40c7b41be12
natan <natangraffi@hotmail.com>
2025-12-19 10:32:24 -0300

    (fix) detalhes

--- src/data/services/dev.service.ts
+++ src/data/services/dev.service.ts
@@ -2,21 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { Postagem } from '../../postagem/entities/postagem.entity';
 import { Usuario } from '../../usuario/entities/usuario.entity';
-import { ConfigService } from '@nestjs/config';
 import { Tema } from '../../tema/entities/tema.entity';
 
 @Injectable()
 export class DevService implements TypeOrmOptionsFactory {
-  constructor(private configService: ConfigService) {}
-
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'mysql',
-      host: this.configService.get<string>('DB_HOST'),
-      port: this.configService.get<number>('DB_PORT'),
-      username: this.configService.get<string>('DB_USERNAME'),
-      password: this.configService.get<string>('DB_PASSWORD'),
-      database: this.configService.get<string>('DB_DATABASE'),
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
       entities: [Postagem, Tema, Usuario],
       synchronize: true,
     };
