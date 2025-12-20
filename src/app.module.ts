6f5579938e44dec55fda2cd3dc3993c3bc389dfe
Natan Graffitti <natangraffi@hotmail.com>
2025-12-19 22:16:56 -0300

    FIX

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
-import { Type } from 'class-transformer';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: ProdService,
       imports: [ConfigModule],
